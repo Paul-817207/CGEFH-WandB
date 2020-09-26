f3326e45223d3c7c090e3c047bd01a9b42efaa56
Paul-817207 <p_hoffer1@hotmail.com>
2020-09-26 09:46:32 -0400

    Initial commit is the Weight and Balance Calculator for C172 C GEFH

---
+++ page_resources/my_EFH_chart_create.js
@@ -0,0 +1,220 @@
+
+//function to zoom the chart in and out
+function zoom(){
+	
+	let chart_canvas = document.getElementById('output_chart');
+	
+	if (chart_canvas.width < 480){
+	  //zoom in to 640 x 480 
+	  chart_canvas.width = 640;
+	  chart_canvas.height = 480;
+	}else{
+      //zoom out to 320x240
+	  chart_canvas.width = 320;
+	  chart_canvas.height = 240;
+	}
+	
+	//redraw to new sizeToContent
+	makeCalculation();
+}
+
+//function to create the chart displaying the CofG data sent to if from the calculated values
+
+function chart_it(TO_weight, TO_CofG, landing_weight, landing_CofG, WandB_warning, fuel_reserve_warning) {
+   
+   //alert('inside chart function warning status is: ' + WandB_warning);
+   //alert('inside chart function. fuel reserve warning is ' + fuel_reserve_warning);
+   
+   let chart_canvas = document.getElementById('output_chart');
+   let canvas_ctx = chart_canvas.getContext('2d');
+
+   const GEFH_IMG = document.getElementById('GEFH_1');
+   const RGB_RED = 'rgb(255,0,0)';
+   const RGB_GREEN = 'rgb(36, 214, 54)';
+   
+   let display_colour = 'rgb(0,255,0)';
+   let tick_length = (chart_canvas.width * 0.025)/2; //set tick length relative to canvas width
+   let Y_axis_length = (chart_canvas.height * 0.9); //set Y axis length relative to canvas height for future scaling
+   let X_axis_length = (chart_canvas.width * 0.9); //let X axis length relative to canvas width for future scaling
+   let X_tick_offset = (X_axis_length / 8); //X ticks spacing
+   let Y_tick_offset = (Y_axis_length / 13); //Y ticks spacing
+   let X_axis_margin = (chart_canvas.width-X_axis_length)/2; //margin between edge of canvas and start of chart relative for future scaling
+   let Y_axis_margin = (chart_canvas.height - Y_axis_length)/2; //magin for Y axis chart for scaling
+   let XY_axis_beginX = X_axis_margin; //set X axis zero X position relative to canvas for future scaling
+   let XY_axis_beginY = Y_axis_margin+Y_axis_length; //set Y axis zero Y position relative to canvas for future scaling
+   let font_ticks = '12px serif';
+   let font_fuelwarning = '18px serif';
+   let font_labels = '15px serif';
+   let disp_count = 0;
+
+   //set warning label colours
+   (WandB_warning) ? display_colour = RGB_RED : display_colour = RGB_GREEN
+   
+   //set font size based on canvas zoom size
+   
+   if (chart_canvas.width > 320){
+	  //set font for large chart size 
+      font_ticks = '12px serif';
+      font_fuelwarning = '18px serif';
+      font_labels = '15px serif';
+   }else{
+      //set font for small chart size
+      font_ticks = '6px serif';
+      font_fuelwarning = '9px serif';
+      font_labels = '8px serif';
+   }
+   
+   //lets get the ball rolling and test the display
+   function drawChart()
+   {
+	  //clear the canvas for a fresh start
+	  canvas_ctx.clearRect(0, 0, chart_canvas.width, chart_canvas.height);
+	  //set default stroke color
+	  canvas_ctx.strokeStyle = 'black';
+	  
+      //background image
+	  canvas_ctx.drawImage(GEFH_IMG, 0, 0, chart_canvas.width, chart_canvas.height);
+
+	  
+	  //draw a translucent white/ maybe yellow
+	  canvas_ctx.fillStyle = ('rgba(255,255,255,0.3');//white
+	  canvas_ctx.fillRect(0, 0, chart_canvas.width, chart_canvas.height);
+	  
+      //draw graph overlay
+	  canvas_ctx.beginPath();
+	  canvas_ctx.lineWidth = 4;
+	  // Y axis line
+	  canvas_ctx.moveTo(XY_axis_beginX, Y_axis_length + tick_length + Y_axis_margin);
+	  canvas_ctx.lineTo(XY_axis_beginX, Y_axis_margin);
+	  // X axis line
+	  canvas_ctx.moveTo(XY_axis_beginX - tick_length, XY_axis_beginY);
+	  canvas_ctx.lineTo(X_axis_length + X_axis_margin, XY_axis_beginY);
+	  
+	  canvas_ctx.stroke();
+	  
+	  //fill in X axis ticks
+	  canvas_ctx.beginPath();
+	  canvas_ctx.lineWidth = 1;
+	  for(let i=1; i<9; i++ ){
+		 //draw the tick 
+		 canvas_ctx.beginPath();
+         canvas_ctx.moveTo(XY_axis_beginX + X_tick_offset*i, XY_axis_beginY + tick_length);
+	     canvas_ctx.lineTo(XY_axis_beginX + X_tick_offset*i, XY_axis_beginY - tick_length);	
+		 canvas_ctx.stroke();
+		 
+      }
+	  disp_count = 10;
+	  canvas_ctx.fillStyle = 'rgb(0,0,0)';	
+	  canvas_ctx.beginPath();
+	  canvas_ctx.textAlign = 'center';
+	  canvas_ctx.font = font_ticks;
+	  canvas_ctx.fillText('8', XY_axis_beginX, XY_axis_beginY + tick_length*2.1);
+	  for(let i=1; i<9; i++ ){
+         //draw the text number below the tick
+		 canvas_ctx.fillText(disp_count.toString(10), XY_axis_beginX + X_tick_offset*i, XY_axis_beginY + tick_length*2);
+		 disp_count = disp_count + 2;
+      }
+	  
+	  //fill Y axis ticks
+	  disp_count = 750;
+	  canvas_ctx.textAlign = 'end';
+	  canvas_ctx.fillText(disp_count.toString(10), XY_axis_beginX - tick_length, XY_axis_beginY); 
+	  for(let i=1; i<14; i++){
+	     //draw the tick 
+		 canvas_ctx.beginPath();
+         canvas_ctx.moveTo(XY_axis_beginX - tick_length, XY_axis_beginY - Y_tick_offset*i);
+	     canvas_ctx.lineTo(XY_axis_beginX + tick_length, XY_axis_beginY - Y_tick_offset*i);	
+		 canvas_ctx.stroke();
+		 disp_count = disp_count + 50;
+		 canvas_ctx.fillText(disp_count.toString(10), XY_axis_beginX - tick_length, XY_axis_beginY - Y_tick_offset*i);
+	  }
+
+	  //create the envelope outline
+	  canvas_ctx.strokeStyle = 'black';
+	  canvas_ctx.fillStyle = 'rgba(36, 214, 54, 0.2)';
+	  canvas_ctx.lineWidth = 2;
+	  //rectangle of lines connecting the corner points of the operating CG envelope
+	  // x point formula = (COFGVALUE *(X_tick_offset/2))-(8*(X_tick_offset/2)) + X_axis_margin
+	  // y point formula = (Y_axis_margin+Y_axis_length) -((WEIGHTVALUE * (Y_tick_offset/50)) - (750*(Y_tick_offset/50)))
+	  canvas_ctx.beginPath();
+	  canvas_ctx.moveTo((10.6 *(X_tick_offset/2))-(8*(X_tick_offset/2)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((1220 * (Y_tick_offset/50)) - (750*(Y_tick_offset/50))));
+	  canvas_ctx.lineTo((22.7 *(X_tick_offset/2))-(8*(X_tick_offset/2)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((1220 * (Y_tick_offset/50)) - (750*(Y_tick_offset/50))));
+	  canvas_ctx.lineTo((22.7 *(X_tick_offset/2))-(8*(X_tick_offset/2)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((750 * (Y_tick_offset/50)) - (750*(Y_tick_offset/50))));
+	  canvas_ctx.lineTo((10.6 *(X_tick_offset/2))-(8*(X_tick_offset/2)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((750 * (Y_tick_offset/50)) - (750*(Y_tick_offset/50))));
+	  canvas_ctx.lineTo((10.6 *(X_tick_offset/2))-(8*(X_tick_offset/2)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((1220 * (Y_tick_offset/50)) - (750*(Y_tick_offset/50))));
+	  canvas_ctx.stroke();
+	  canvas_ctx.fill();
+	  
+	  //Show fuel warning at top of chart in RED to alert when reserve is lower than about 30 minutes cruise
+	  canvas_ctx.textAlign = 'start';
+	  if(fuel_reserve_warning){
+	  canvas_ctx.fillStyle = RGB_RED;
+	  canvas_ctx.font = font_fuelwarning;
+	  canvas_ctx.fillText ('FUEL RESERVE WARNING, estimated fuel reserve is less than 30 minutes!',
+		XY_axis_beginX + X_axis_margin/2, Y_axis_margin*2);
+	  }
+	  
+	  //set display color to warning level
+	  canvas_ctx.fillStyle = display_colour;
+	  
+
+	  //calculate Take Off point for plotting to the chart
+	  //X axis calculation X_tick_offset  = 90. 90/2 = 45 = 1 inch CG. so 10 in CG = (10 * (X_tick_offset/2)) - (8*(X_tick_offset/2)) + xmargin	  
+	  let TO_xvalue = (TO_CofG *(X_tick_offset/2))-(8*(X_tick_offset/2)) + X_axis_margin;
+	  //Y axis calculation (TO_weight * (Y_tick_offset/50)) - (750*(Y_tick_offset/50)) + Y_axis_margin *-1
+	  let TO_yvalue = (Y_axis_margin+Y_axis_length) -((TO_weight * (Y_tick_offset/50)) - (750*(Y_tick_offset/50)) );
+
+	  //Paint the take off Weight and Balance to the chart
+	  canvas_ctx.font = font_labels;
+      canvas_ctx.beginPath();  
+      canvas_ctx.arc( TO_xvalue, TO_yvalue, 4, 0, 2*Math.PI,false); 
+	  canvas_ctx.fill();
+	  //label the take off point
+	  canvas_ctx.fillStyle = 'black';
+	  canvas_ctx.fillText(' __Take Off Weight & Balance point',TO_xvalue, TO_yvalue);
+	  
+	  //calculate Landing point for plotting to the chart
+	  //X axis calculation X_tick_offset  = 90. 90/2 = 45 = 1 inch CG. so 10 in CG = (10 * (X_tick_offset/2)) - (8*(X_tick_offset/2)) + xmargin	  
+	  let LDG_xvalue = (landing_CofG *(X_tick_offset/2))-(8*(X_tick_offset/2)) + X_axis_margin;
+	  //Y axis calculation (TO_weight * (Y_tick_offset/50)) - (750*(Y_tick_offset/50)) + Y_axis_margin *-1
+	  let LDG_yvalue = (Y_axis_margin+Y_axis_length) -((landing_weight * (Y_tick_offset/50)) - (750*(Y_tick_offset/50)) );
+	  
+	  //Paint the Landing Weight and Balance to the chart
+	  canvas_ctx.fillStyle = display_colour;
+	  canvas_ctx.beginPath();  
+      canvas_ctx.arc( LDG_xvalue, LDG_yvalue, 4, 0, 2*Math.PI,false); 
+	  canvas_ctx.fill();
+	  //label the take off point
+	  canvas_ctx.fillStyle = 'black';
+	  //displace display if too close together
+	  if (chart_canvas.width > 320){
+	     //set label offset for large chart size 
+	     if(( LDG_yvalue - TO_yvalue)>14){
+		   canvas_ctx.fillText(' __Landing Weight & Balance point',LDG_xvalue, LDG_yvalue);
+	     }else{
+		    canvas_ctx.fillText(' ~~Landing Weight & Balance point',LDG_xvalue, LDG_yvalue + 14); 
+		 }
+	  }else{
+	     //set label offset for small chart size 
+	     if(( LDG_yvalue - TO_yvalue)>7){
+		   canvas_ctx.fillText(' __Landing Weight & Balance point',LDG_xvalue, LDG_yvalue);
+	     }else{
+		    canvas_ctx.fillText(' ~~Landing Weight & Balance point',LDG_xvalue, LDG_yvalue + 7);
+	     }
+	  }
+	  
+	  //draw a line on the chart connecting the two points
+	  canvas_ctx.strokeStyle = display_colour;
+	  canvas_ctx.beginPath();
+	  canvas_ctx.lineWidth = 2;
+	  canvas_ctx.moveTo(TO_xvalue, TO_yvalue);
+	  canvas_ctx.lineTo(LDG_xvalue, LDG_yvalue); 
+	  canvas_ctx.stroke();
+	  
+   }
+
+
+   
+   //make it do number onerror
+   drawChart();
+}
---
+++ page_resources/page_js_code.js
@@ -0,0 +1,338 @@
+//function to convert litres to usg and put litres value in usg field
+function convert_litres(tank){
+	
+	//alert('in convert function'+tank);
+	if(tank === 'main_usg'){
+		if (!isNaN(parseFloat(document.getElementById("main_fuel_usg").value, 10)))
+			document.getElementById("main_fuel_litres").value = parseFloat((parseFloat(document.getElementById("main_fuel_usg").value, 10)/0.26417).toFixed(1));
+	}
+	if(tank === 'main_litres'){
+		if (!isNaN(parseFloat(document.getElementById("main_fuel_litres").value, 10))){
+			document.getElementById("main_fuel_usg").value = parseFloat((parseFloat(document.getElementById("main_fuel_litres").value, 10)*0.26417).toFixed(1));
+		}else{
+			document.getElementById("main_fuel_usg").value = 0;
+		}
+	}
+	if(tank === 'burn_usg'){
+		if (!isNaN(parseFloat(document.getElementById("est_fuel_burn_usgph").value, 10))){
+			document.getElementById("est_fuel_burn_lph").value = parseFloat((parseFloat(document.getElementById("est_fuel_burn_usgph").value, 10)/0.26417).toFixed(1));
+		}else{
+			document.getElementById("est_fuel_burn_lph").value = 0;
+		}
+	}
+	if(tank === 'burn_litres'){
+		if (!isNaN(parseFloat(document.getElementById("est_fuel_burn_lph").value, 10))){
+			document.getElementById("est_fuel_burn_usgph").value = parseFloat((parseFloat(document.getElementById("est_fuel_burn_lph").value, 10)*0.26417).toFixed(1));
+		}else{
+			document.getElementById("est_fuel_burn_usgph").value = 0;
+		}
+	}
+	if(tank === 'est_fuel_usg'){
+		if (!isNaN(parseFloat(document.getElementById("flight_fuel_used_usg").value, 10))){
+			document.getElementById("estimated_fuel_litres").value = parseFloat((parseFloat(document.getElementById("flight_fuel_used_usg").value, 10)/0.26417).toFixed(1));
+		}else{
+			document.getElementById("estimated_fuel_litres").value = 0;
+		}
+	
+	}
+		if(tank === 'est_fuel_litres'){
+		if (!isNaN(parseFloat(document.getElementById("estimated_fuel_litres").value, 10))){
+			document.getElementById("flight_fuel_used_usg").value = parseFloat((parseFloat(document.getElementById("estimated_fuel_litres").value, 10)*0.26417).toFixed(1));
+		}else{
+			document.getElementById("flight_fuel_used_usg").value = 0;
+		}
+	}
+	//After conversion is complete, call makeCalculation() to calculate and display the change
+	makeCalculation();
+}
+
+function makeCalculation() { 
+  // Constants specific to C-GEFH
+  const MAX_GROSS_WEIGHT = 1220;
+  const WHEEL_BASIC_EMPTY_WEIGHT = 770;
+  const SKI_BASIC_EMPTY_WEIGHT = 782;
+  const AIRCRAFT_ARM_WHEELS = 13.75;//10.75;
+  const AIRCRAFT_ARM_SKIS = 13.59;//10.63;
+  const OIL_QUART_LBS = 1.875;
+  const FUEL_USG_LBS = 6.0;
+  const FUEL_BURN_USGPH = 10; //estimated US gallons per hour of fuel burn of this engine at cruise power setting
+  const PILOT_ARM = 36.0;
+  const PASS_ARM = 9.0;
+  const BAGGAGE_ARM = 49.0;
+  const FUEL_MAIN_ARM = (-18.0);
+  const FUEL_WING_ARM = 24.0;
+  const OIL_ARM = (-29.0);
+  const ARM_MIN = 10.6;
+  const ARM_MAX = 22.7;
+  
+  //variables to make the calculations
+  let is_over_Gross_TO_weight = false;
+  let is_over_Gross_LDG_weight = false;
+  let is_outside_TO_CG_envelope = false;
+  let is_outside_LDG_CG_envelope = false;
+  let is_below_minimum_fuel_reserve = false;
+  let take_off_weight = 0;
+  let landing_weight = 0;
+  let temp_value = 0.0;
+  let gear_type = "";
+  let pilot_lbs = 0;
+  let pass_lbs = 0;
+  let baggage_lbs = 0;
+  let main_fuel_lbs = 0;
+  let wing_fuel_lbs = 0;
+  let oil_lbs = 0;
+  let fuel_used_for_flight_lbs = 0;
+  let main_fuel_volume_usg = 0.0;
+  let wing_fuel_volume_usg = 0.0;
+  let landing_main_fuel_usg = 0.0;
+  let landing_wing_fuel_usg = 0.0;
+  let landing_fuel_reserve_minutes = 0;
+  let fuel_for_flight_usg = 0.0;
+  let take_off_moment = 0.0;
+  let landing_moment = 0.0;
+  let CG_takeoff = 0.0;
+  let CG_landing = 0.0;
+  
+  
+  //collect date entered into the form
+  
+  
+  //get pilot weight from the form
+  temp_value = parseInt(document.getElementById("pilot_weight").value, 10);
+  if (isNaN(temp_value) || temp_value<0) 
+  {
+     //alert invalid entry
+	 temp_value = 0;
+	 document.getElementById("pilot_lbs_error").innerHTML = " ERROR entering pilot weight. Valid entry must be a positive number ";
+  }else{
+    //valid number has been entered
+    pilot_lbs = temp_value;
+	document.getElementById("pilot_lbs_error").innerHTML = "";
+  }
+  
+  //get passenger weight from the form
+  temp_value = parseInt(document.getElementById("pass_weight").value, 10);
+  if (isNaN(temp_value) || temp_value < 0) 
+  {
+     //alert invalid entry
+     temp_value = 0;	 
+	 document.getElementById("pass_lbs_error").innerHTML = " ERROR entering passenger weight. Valid entry must be a positive number ";
+  }else{
+    //valid number has been entered
+    pass_lbs = temp_value;
+	document.getElementById("pass_lbs_error").innerHTML = "";
+  }
+  
+  //get the baggage weight from the form
+  temp_value = parseInt(document.getElementById("baggage_weight").value, 10);
+  if (isNaN(temp_value) || temp_value < 0 || temp_value > 20) 
+  {
+     //alert invalid entry
+     temp_value = 0;	 
+	 document.getElementById("baggage_lbs_error").innerHTML = " ERROR entering baggage weight. Valid entry must be a positive number 20lbs or less";
+  }else{
+    //valid number has been entered
+    baggage_lbs = temp_value;
+	document.getElementById("baggage_lbs_error").innerHTML = "";
+  }
+  
+  //get fuel quantity from the main fuel tank input, verify valid, and calculate weight from the usg volume
+  temp_value = parseFloat(document.getElementById("main_fuel_usg").value, 10);
+  if (isNaN(temp_value) || temp_value < 0 || temp_value > 10) 
+  {
+     //alert invalid entry
+     temp_value = 0;	 
+	 document.getElementById("main_fuel_error").innerHTML = " ERROR entering Main fuel tank USG. Valid entry must be a positive number up to 10 USG";
+  }else{
+    //valid number has been entered
+    main_fuel_volume_usg = temp_value;
+	main_fuel_lbs = main_fuel_volume_usg * FUEL_USG_LBS;
+	document.getElementById("main_fuel_error").innerHTML = "";
+  }
+  
+  //get fuel quantity from the wing fuel tank input, verify valid, and calculate weight from the usg volume
+  temp_value = parseFloat(document.getElementById("wing_fuel_usg").value, 10);
+  if (isNaN(temp_value) || temp_value < 0 || temp_value > 8) 
+  {
+     //alert invalid entry
+     temp_value = 0; 
+	 document.getElementById("wing_fuel_error").innerHTML = " ERROR entering Wing fuel tank USG. Valid entry must be a positive number up to 8 USG";
+  }else{
+    //valid number has been entered
+    wing_fuel_volume_usg = temp_value;
+	wing_fuel_lbs = wing_fuel_volume_usg * FUEL_USG_LBS;
+	document.getElementById("wing_fuel_error").innerHTML = "";
+  }
+  
+  //get oil quantity in engine sump and calcuate weight (amount over or under 4 quarts) with min 2 quart and max 5 quart limits
+  temp_value = parseFloat(document.getElementById("oil_quarts").value, 10);
+  if (isNaN(temp_value) || temp_value < 2 || temp_value > 5) 
+  {
+     //alert invalid entry
+     temp_value = 0;
+	 document.getElementById("oil_error").innerHTML = " ERROR entering oil level in engine. Valid entry must be between 2 and 5 quarts";
+  }else{
+    //valid number has been entered
+    oil_lbs = (temp_value - 4) * OIL_QUART_LBS; //1.875; //calculate the weight of engine oil above or below the basic empty wight value of 4 quarts (1.875lbs/qt)
+	document.getElementById("oil_error").innerHTML = "";
+  }
+
+  //get the value input for the USgallons expected to be used for the flight
+  temp_value = parseFloat(document.getElementById("flight_fuel_used_usg").value, 10);
+  if (isNaN(temp_value) || temp_value < 0 || (temp_value > (main_fuel_volume_usg + wing_fuel_volume_usg)) ) //invalid if less than 0 fuel will be used or more than the total possible capacity of 18usg
+  {
+     //alert invalid entry
+     temp_value = 0; 
+	 document.getElementById("fuel_for_flight_error").innerHTML = " ERROR This value must be between 0 and the combined quantity of the Main and Wing tanks";
+  }else{
+    //valid number has been entered
+	fuel_for_flight_usg = temp_value;
+    fuel_used_for_flight_lbs = fuel_for_flight_usg * FUEL_USG_LBS;// calculate the weight of the estimated fuel used for flight 
+	document.getElementById("fuel_for_flight_error").innerHTML = "";
+  }
+  
+  //total up weights for take off weight
+  if (gear_type === "WHEELS")
+  {
+    take_off_weight = WHEEL_BASIC_EMPTY_WEIGHT + pilot_lbs + pass_lbs + baggage_lbs + main_fuel_lbs + wing_fuel_lbs + oil_lbs;
+	landing_weight = take_off_weight - fuel_used_for_flight_lbs;
+  }else{//must be on skis if it is not on wheels
+    take_off_weight = SKI_BASIC_EMPTY_WEIGHT + pilot_lbs + pass_lbs + baggage_lbs + main_fuel_lbs + wing_fuel_lbs + oil_lbs;
+	landing_weight = take_off_weight - fuel_used_for_flight_lbs;
+  }
+  
+  //calculate Centre of Gravity
+  //first, total all of the weights to get take_off_weight
+  //second, calculate the MOMENT from each individual weight ARM (weight x arm = moment)
+  //third, calculate the sum of all of the individual MOMENTs
+  //fouth, calculate the CofG (CofG = sum of all MOMENTs / sum of all weights
+  
+  //sum up moments for take off
+  take_off_moment = landing_moment = 0;
+  
+  if (gear_type === "WHEELS")
+  {
+    take_off_moment += WHEEL_BASIC_EMPTY_WEIGHT * AIRCRAFT_ARM_WHEELS;
+  }else{//then its skis
+    take_off_moment += SKI_BASIC_EMPTY_WEIGHT * AIRCRAFT_ARM_SKIS;
+  }
+  
+  if(pilot_lbs > 0)
+    take_off_moment += pilot_lbs * PILOT_ARM;
+  if(pass_lbs > 0)
+    take_off_moment += pass_lbs * PASS_ARM;
+  if(baggage_lbs > 0)
+    take_off_moment += baggage_lbs * BAGGAGE_ARM;
+  if(oil_lbs > 0 || oil_lbs < 0)
+	take_off_moment += oil_lbs * OIL_ARM;
+
+  //landing and take off moment calculation is the same to this point so make them equal
+  landing_moment = take_off_moment;
+
+  if(main_fuel_lbs > 0)
+    //take off moment
+    take_off_moment += main_fuel_lbs * FUEL_MAIN_ARM;
+	//landing moment ( fuel minus fuel used for flight )
+  if(wing_fuel_lbs > 0)
+    take_off_moment += wing_fuel_lbs * FUEL_WING_ARM;
+	//landing moment (fuel minus fuel used for flight)
+
+  //calculate and estimate of landing configuration. it complicated by the choice the pilot makes
+  //about how much fuel, if any, is used from the wing tank.
+  //for the purpose of this calculation, it will be assumed that the fuel will be used from the wing 
+  //tank and drained into the main tank before landing.
+  
+  //check if any fuel has been entered in the wing tank, if none, then skip to the next step
+  if(wing_fuel_lbs > 0)
+  {
+	  //check if fuel estimated for flight is greater than the amount entered in the wing tank. if so, then the wing tank will be assumed to be empty at landing
+	  if (fuel_for_flight_usg < wing_fuel_volume_usg)
+	  {
+		  landing_main_fuel_usg = main_fuel_volume_usg;
+		  landing_wing_fuel_usg = wing_fuel_volume_usg - fuel_for_flight_usg;
+		  landing_moment += (landing_wing_fuel_usg * FUEL_USG_LBS) * FUEL_WING_ARM;
+		  landing_moment += main_fuel_lbs * FUEL_MAIN_ARM;
+	  }else{//all used from the wing therefore check and calculate how much also used from the main tank
+		  landing_wing_fuel_usg = 0;
+		  landing_main_fuel_usg = (main_fuel_volume_usg - (fuel_for_flight_usg - wing_fuel_volume_usg));
+		  landing_moment += landing_main_fuel_usg * FUEL_USG_LBS * FUEL_MAIN_ARM;
+	  }
+  }else{//no fuel in wing on take off therefore all fuel used will be from the main tank, calcuate the landing fuel weight CG
+	  landing_main_fuel_usg = main_fuel_volume_usg - fuel_for_flight_usg;
+	  landing_moment += (main_fuel_lbs - fuel_used_for_flight_lbs) * FUEL_MAIN_ARM;
+  }  
+	
+  //calculate take off C of G
+
+  //calculate the Centre of Gravity for take off from the above provided data
+  CG_takeoff = take_off_moment / take_off_weight;
+  //calculate the Centre of Gravity for landing from the above provided data
+  CG_landing = landing_moment / landing_weight;
+	
+  //ok, now all of the calculations are made, it is time to test the results to see if they are in the 
+  //flight envelope or not and VERY CLEARLY display the results to the user, both in text and 
+  //image form. people like pictures
+  
+  //check if the gross weight or the CofG is out of the safe envelope and set a flag to highlight that fact in RED
+  if ( take_off_weight > MAX_GROSS_WEIGHT ){
+	  is_over_TO_Gross_weight = true;
+  }else{
+	  is_over_TO_Gross_weight = false;
+  }
+  
+  if ( landing_weight > MAX_GROSS_WEIGHT ) {
+	  is_over_Gross_LDG_weight = true;
+  }else{
+	  is_over_Gross_LDG_weight = false;
+  }
+
+   if ( CG_takeoff < 10.6 || CG_takeoff > 22.7 ){
+	   is_outside_TO_CG_envelope = true;
+   }else{
+	   is_outside_TO_CG_envelope = false;
+   }
+   
+   if ( CG_landing < 10.6 || CG_landing > 22.7 ){
+	   is_outside_LDG_CG_envelope = true;
+   }else{
+	   is_outside_LDG_CG_envelope = false;
+   }
+  //check to see of there is enough of a fuel reserve. Approx 2.5usg for 30 minutes and set a flag if it is not.
+  landing_fuel_reserve_minutes = (landing_main_fuel_usg + landing_wing_fuel_usg) * (60 / FUEL_BURN_USGPH);
+  if(landing_fuel_reserve_minutes < 30){
+	  is_below_minimum_fuel_reserve = true;
+  }else{
+	  is_below_minimum_fuel_reserve = false;
+  } 
+  
+  
+  //output the results to the screen
+  if (true)//valid results on the entry form
+  {
+    document.getElementById("output").innerHTML =  
+    "<p><br>"
+	+ "Take off configuration:<br>"   
+	+ "&nbsp&nbsp&nbspMain fuel tank contains&nbsp" + main_fuel_volume_usg + "&nbspusg that weighs&nbsp" + main_fuel_lbs.toFixed(2) + "&nbsplbs<br>"
+	+ "&nbsp&nbsp&nbspWing fuel tank contains&nbsp" + wing_fuel_volume_usg + "&nbspusg that weighs&nbsp" + wing_fuel_lbs.toFixed(2) + "&nbsplbs<br>"
+	+ "&nbsp&nbsp&nbspTake off weight (max =1220lbs) =&nbsp " + (take_off_weight).toFixed(0) + "&nbsplbs&nbsp" + (is_over_TO_Gross_weight ? '<font color="red"><span>is over max gross weight. WARNING</span></font><br>':'<font color="green"><span>&nbspis OK</span></font><br>')
+	+ "&nbsp&nbsp&nbspTake off Centre of Gravity (min 10.6 max 22.7) =&nbsp" + CG_takeoff.toFixed(2) + (is_outside_TO_CG_envelope ? '<font color="red"><span>&nbspOutside of CG envelope. WARNING</span></font><br><br>' : '<font color="green"><span>&nbspis OK</span></font><br>')
+	+ "&nbsp&nbsp&nbspRemaining Load to gross weight is " + (MAX_GROSS_WEIGHT - take_off_weight).toFixed(0) + "&nbsplbs<br><br>"
+	
+	+ "Estimated landing configuration:<br>"
+	+ "&nbsp&nbsp&nbspReserve fuel time is about &nbsp" + landing_fuel_reserve_minutes + "&nbspminutes calculated at&nbsp" + FUEL_BURN_USGPH + "&nbspusgph&nbsp" + ( (is_below_minimum_fuel_reserve) ?  '<font color="red"><span>WARNING, less than 30 minutes of fuel remaining at landing</span></font><br>' : '<font color="green"><span>&nbspis OK</span></font><br>' )
+	+ "&nbsp&nbsp&nbspFuel MAIN tank level at landing is &nbsp" + landing_main_fuel_usg + "&nbspusg that weighs &nbsp" + (landing_main_fuel_usg * FUEL_USG_LBS).toFixed(2) + "&nbsplbs<br>"
+	+ "&nbsp&nbsp&nbspFuel WING tank level at landing is &nbsp" + landing_wing_fuel_usg + "&nbspusg that weighs &nbsp" + (landing_wing_fuel_usg * FUEL_USG_LBS).toFixed(2) + "&nbsplbs<br>"
+	+ "&nbsp&nbsp&nbspLanding weight (max =1220lbs) =&nbsp" + (landing_weight).toFixed(0) + "&nbsplbs&nbsp" + (is_over_Gross_LDG_weight ? '<font color="red"><span>is over max gross weight. WARNING</span></font><br>':'<font color="green"><span>is OK<br></font>')
+	+ "&nbsp&nbsp&nbspLanding Centre of Gravity (min 10.6 max 22.7) = &nbsp" + CG_landing.toFixed(2) + (is_outside_LDG_CG_envelope ? '<font color="red"><span>&nbspOutside of CG envelope. WARNING</span></font><br>':'<font color="green"><span>&nbspis OK</span></font><br>')
+    
+    + "</p>";
+  }
+  else//invalid information on entry form
+  {
+    document.getElementById("output").innerHTML = "INVALID form data, Please complete the form or correct any mistakes entered";
+  }
+  
+  //send the data to the chart function to display in chart form
+  chart_it(take_off_weight, CG_takeoff, landing_weight, CG_landing,(is_outside_TO_CG_envelope || is_outside_LDG_CG_envelope || is_over_Gross_LDG_weight || is_over_TO_Gross_weight), is_below_minimum_fuel_reserve);
+  
+}
