d3f5ba96c0b0081a5d6470a91644a96488f9f97e
Paul-817207 <p_hoffer1@hotmail.com>
2020-10-04 09:28:17 -0400

    progressive changes in converstion from SBM to EFH

--- page_resources/page_js_code.js
+++ page_resources/page_js_code.js
@@ -59,14 +59,13 @@ function makeCalculation() {
   const PILOT_F_PASS_ARM = 37.0;//Pilot and front passenger arm " aft of datum in normal seated position. (34.0" to 46.0" seat adjust range)
   const PASS_ARM_REAR = 73.0;//rear passenger arm " aft of datum
   const BAGGAGE_1_ARM = 95.0;//Baggage area #1 arm " aft of datum 120 lbs max
-  const BAGGAGE_2_ARM = 123.0;//Baggage area #2 arm " aft of datum 50 lbs max PLUS Baggage are 1 & 2 max 120 lbs
-   
-  const FUEL_MAIN_ARM = (0.0);//Long range fuel tanks. 48usg useable
-
+  const BAGGAGE_2_ARM = 123.0;//Baggage area #2 arm " aft of datum 50 lbs max PLUS Baggage are 1 & 2 max 120 lbs   
+  const FUEL_MAIN_ARM = (48.0);//Long range fuel tanks. 48usg useable
   const OIL_ARM = (-13.3);//8 qts = 15 lbs can be used for all calculations
+X--not quite sure how I am going to deal with this yet
   const ARM_MIN = 10.6;
   const ARM_MAX = 22.7;
-X  
+X---
   //variables to make the calculations
   let is_over_Gross_TO_weight = false;
   let is_over_Gross_LDG_weight = false;
@@ -76,18 +75,16 @@ X
   let take_off_weight = 0;
   let landing_weight = 0;
   let temp_value = 0.0;
-  let gear_type = "";
-  let pilot_lbs = 0;
-  let pass_lbs = 0;
-  let baggage_lbs = 0;
-  let main_fuel_lbs = 0;
-  let wing_fuel_lbs = 0;
+  let pilot_frt_pass_lbs = 0; 
+  let pass_rear_lbs = 0;
+  let baggage1_lbs = 0;
+  let baggage2_lbs = 0;
+  let main_fuel_lbs = 0;  
   let oil_lbs = 0;
+  let entered_fuel_burn_usgph = 0;
   let fuel_used_for_flight_lbs = 0;
   let main_fuel_volume_usg = 0.0;
-  let wing_fuel_volume_usg = 0.0;
   let landing_main_fuel_usg = 0.0;
-  let landing_wing_fuel_usg = 0.0;
   let landing_fuel_reserve_minutes = 0;
   let fuel_for_flight_usg = 0.0;
   let take_off_moment = 0.0;
@@ -99,52 +96,65 @@ X
   //collect date entered into the form
   
   
-  //get pilot weight from the form
-  temp_value = parseInt(document.getElementById("pilot_weight").value, 10);
+  //get pilot and front passenger weight from the form
+  temp_value = parseInt(document.getElementById("pilot_fpx_weight").value, 10);
   if (isNaN(temp_value) || temp_value<0) 
   {
      //alert invalid entry
 	 temp_value = 0;
-	 document.getElementById("pilot_lbs_error").innerHTML = " ERROR entering pilot weight. Valid entry must be a positive number ";
+	 document.getElementById("pilot_fpx_lbs_error").innerHTML = " ERROR, Must be a positive number ";
   }else{
     //valid number has been entered
-    pilot_lbs = temp_value;
-	document.getElementById("pilot_lbs_error").innerHTML = "";
+    pilot_frt_pass_lbs = temp_value;
+	document.getElementById("pilot_fpx_lbs_error").innerHTML = "";
   }
-  
-  //get passenger weight from the form
-  temp_value = parseInt(document.getElementById("pass_weight").value, 10);
+
+  //get rear passenger weight from the form
+  temp_value = parseInt(document.getElementById("pass_rear_weight").value, 10);
   if (isNaN(temp_value) || temp_value < 0) 
   {
      //alert invalid entry
      temp_value = 0;	 
-	 document.getElementById("pass_lbs_error").innerHTML = " ERROR entering passenger weight. Valid entry must be a positive number ";
+	 document.getElementById("pass_rear_lbs_error").innerHTML = " ERROR, Must be a positive number ";
   }else{
     //valid number has been entered
-    pass_lbs = temp_value;
-	document.getElementById("pass_lbs_error").innerHTML = "";
+    pass_rear_lbs = temp_value;
+	document.getElementById("pass_rear_lbs_error").innerHTML = "";
   }
-  
-  //get the baggage weight from the form
-  temp_value = parseInt(document.getElementById("baggage_weight").value, 10);
-  if (isNaN(temp_value) || temp_value < 0 || temp_value > 20) 
+
+  //get the baggage area #1 weight from the form (120 lbs or less)
+  temp_value = parseInt(document.getElementById("baggage1_weight").value, 10);
+  if (isNaN(temp_value) || temp_value < 0 || temp_value > 120) 
   {
      //alert invalid entry
      temp_value = 0;	 
-	 document.getElementById("baggage_lbs_error").innerHTML = " ERROR entering baggage weight. Valid entry must be a positive number 20lbs or less";
+	 document.getElementById("baggage1_lbs_error").innerHTML = " ERROR, Must be a positive number 120 lbs or less";
   }else{
     //valid number has been entered
-    baggage_lbs = temp_value;
-	document.getElementById("baggage_lbs_error").innerHTML = "";
+    baggage1_lbs = temp_value;
+	document.getElementById("baggage1_lbs_error").innerHTML = "";
   }
-  
+
+  //get the baggage area #2 weight from the form
+  temp_value = parseInt(document.getElementById('baggage2_weight').value, 10);
+  if (isNaN(temp_value) || temp_value < 0 || temp_value > 50 || (temp_value + baggage1_lbs) > 120)
+  {
+	  //alert invalid entry
+	  temp_value = 0;
+	  document.getElementById('baggage2_lbs_error').innerHTML = 'ERROR, Must be a positive number & Baggage area #1 + #2 cannot exceed 120 lbs';
+  }else{
+	  //valid OK entry value
+	  baggage2_lbs = temp_value;
+	  document.getElementById('baggage2_lbs_error').innerHTML = '';
+  }	  
+
   //get fuel quantity from the main fuel tank input, verify valid, and calculate weight from the usg volume
   temp_value = parseFloat(document.getElementById("main_fuel_usg").value, 10);
-  if (isNaN(temp_value) || temp_value < 0 || temp_value > 10) 
+  if (isNaN(temp_value) || temp_value < 0 || temp_value > 48) 
   {
      //alert invalid entry
      temp_value = 0;	 
-	 document.getElementById("main_fuel_error").innerHTML = " ERROR entering Main fuel tank USG. Valid entry must be a positive number up to 10 USG";
+	 document.getElementById("main_fuel_error").innerHTML = " ERROR, Must be a positive number up to 48 USG";
   }else{
     //valid number has been entered
     main_fuel_volume_usg = temp_value;
@@ -152,54 +162,40 @@ X
 	document.getElementById("main_fuel_error").innerHTML = "";
   }
   
-  //get fuel quantity from the wing fuel tank input, verify valid, and calculate weight from the usg volume
-  temp_value = parseFloat(document.getElementById("wing_fuel_usg").value, 10);
-  if (isNaN(temp_value) || temp_value < 0 || temp_value > 8) 
+  //get the entered estimated fuel burn rate in US gallons per hour
+  temp_value = parseFloat(document.getElementById('est_fuel_burn_usgph').value, 10);
+  if (isNaN(temp_value) || temp_value < 0) 
   {
-     //alert invalid entry
-     temp_value = 0; 
-	 document.getElementById("wing_fuel_error").innerHTML = " ERROR entering Wing fuel tank USG. Valid entry must be a positive number up to 8 USG";
+    //not a valid entry
+	temp_value = 0;	 
+	document.getElementById("fuel_burn_error").innerHTML = 'ERROR, Must be a positive number';
   }else{
-    //valid number has been entered
-    wing_fuel_volume_usg = temp_value;
-	wing_fuel_lbs = wing_fuel_volume_usg * FUEL_USG_LBS;
-	document.getElementById("wing_fuel_error").innerHTML = "";
+    //valid entry !
+    entered_fuel_burn_usgph = temp_value;
+	document.getElementById("fuel_burn_error").innerHTML = '';
   }
-  
-  //get oil quantity in engine sump and calcuate weight (amount over or under 4 quarts) with min 2 quart and max 5 quart limits
-  temp_value = parseFloat(document.getElementById("oil_quarts").value, 10);
-  if (isNaN(temp_value) || temp_value < 2 || temp_value > 5) 
-  {
-     //alert invalid entry
-     temp_value = 0;
-	 document.getElementById("oil_error").innerHTML = " ERROR entering oil level in engine. Valid entry must be between 2 and 5 quarts";
-  }else{
-    //valid number has been entered
-    oil_lbs = (temp_value - 4) * OIL_QUART_LBS; //1.875; //calculate the weight of engine oil above or below the basic empty wight value of 4 quarts (1.875lbs/qt)
-	document.getElementById("oil_error").innerHTML = "";
-  }
-
+ 
   //get the value input for the USgallons expected to be used for the flight
   temp_value = parseFloat(document.getElementById("flight_fuel_used_usg").value, 10);
-  if (isNaN(temp_value) || temp_value < 0 || (temp_value > (main_fuel_volume_usg + wing_fuel_volume_usg)) ) //invalid if less than 0 fuel will be used or more than the total possible capacity of 18usg
+  if (isNaN(temp_value) || temp_value < 0 || (temp_value > 48) ) //invalid if less than 0 fuel will be used or more than the total possible capacity of 48usg
   {
      //alert invalid entry
      temp_value = 0; 
-	 document.getElementById("fuel_for_flight_error").innerHTML = " ERROR This value must be between 0 and the combined quantity of the Main and Wing tanks";
+	 document.getElementById("fuel_for_flight_error").innerHTML = " ERROR, Must be between 0 and 48 USG";
   }else{
     //valid number has been entered
 	fuel_for_flight_usg = temp_value;
     fuel_used_for_flight_lbs = fuel_for_flight_usg * FUEL_USG_LBS;// calculate the weight of the estimated fuel used for flight 
 	document.getElementById("fuel_for_flight_error").innerHTML = "";
   }
-  
+X---OK
   //total up weights for take off weight
   if (gear_type === "WHEELS")
   {
-    take_off_weight = WHEEL_BASIC_EMPTY_WEIGHT + pilot_lbs + pass_lbs + baggage_lbs + main_fuel_lbs + wing_fuel_lbs + oil_lbs;
+    take_off_weight = WHEEL_BASIC_EMPTY_WEIGHT + pilot_frt_pass_lbs + pass_rear_lbs + baggage1_lbs + main_fuel_lbs + wing_fuel_lbs + oil_lbs;
 	landing_weight = take_off_weight - fuel_used_for_flight_lbs;
   }else{//must be on skis if it is not on wheels
-    take_off_weight = SKI_BASIC_EMPTY_WEIGHT + pilot_lbs + pass_lbs + baggage_lbs + main_fuel_lbs + wing_fuel_lbs + oil_lbs;
+    take_off_weight = SKI_BASIC_EMPTY_WEIGHT + pilot_frt_pass_lbs + pass_rear_lbs + baggage1_lbs + main_fuel_lbs + wing_fuel_lbs + oil_lbs;
 	landing_weight = take_off_weight - fuel_used_for_flight_lbs;
   }
   
@@ -219,12 +215,12 @@ X
     take_off_moment += SKI_BASIC_EMPTY_WEIGHT * AIRCRAFT_ARM_SKIS;
   }
   
-  if(pilot_lbs > 0)
-    take_off_moment += pilot_lbs * PILOT_F_PASS_ARM;
-  if(pass_lbs > 0)
-    take_off_moment += pass_lbs * PASS_ARM_REAR;
-  if(baggage_lbs > 0)
-    take_off_moment += baggage_lbs * BAGGAGE_1_ARM;
+  if(pilot_frt_pass_lbs > 0)
+    take_off_moment += pilot_frt_pass_lbs * PILOT_F_PASS_ARM;
+  if(pass_rear_lbs > 0)
+    take_off_moment += pass_rear_lbs * PASS_ARM_REAR;
+  if(baggage1_lbs > 0)
+    take_off_moment += baggage1_lbs * BAGGAGE_1_ARM;
   if(oil_lbs > 0 || oil_lbs < 0)
 	take_off_moment += oil_lbs * OIL_ARM;
 
