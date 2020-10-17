b95b8bec9873a42b49b8cb698c86297488cdf0f0
Paul-817207 <p_hoffer1@hotmail.com>
2020-10-16 20:59:09 -0400

    refactored the chart generation function to make it a little more generic and easier to adjust the chart values from defined constants at the beginning of the function

--- page_resources/my_EFH_chart_create.js
+++ page_resources/my_EFH_chart_create.js
@@ -22,8 +22,16 @@ function zoom(){
 
 function chart_it(TO_weight, TO_CofG, landing_weight, landing_CofG, WandB_warning, fuel_reserve_warning) {
    
-   //alert('inside chart function warning status is: ' + WandB_warning);
-   //alert('inside chart function. fuel reserve warning is ' + fuel_reserve_warning);
+   //****************************************************************************************
+   //in this part, some constants that define the chart X and Y properties to fit individual aircraft types
+   const Y_AXIS_START_WEIGHT = 1500;//	starting wight value for the Y axis
+   const Y_AXIS_INCREMENT_VALUE = 100;// The value that the Y axis is incremented by
+   const Y_AXIS_NUMBEROFTICS = 10;// Number of tics in the Y axis
+   
+   const X_AXIS_START_INCHES = 34;// Number to start the X axis inches centre of gravity at
+   const X_AXIS_INCREMENT_VALUE = 1;// The value that the X axis is incemented by
+   const X_AXIS_NUMBEROFTICS = 15;// Number of tics in the X axis
+   //****************************************************************************************
    
    let chart_canvas = document.getElementById('output_chart');
    let canvas_ctx = chart_canvas.getContext('2d');
@@ -36,8 +44,8 @@ function chart_it(TO_weight, TO_CofG, landing_weight, landing_CofG, WandB_warnin
    let tick_length = (chart_canvas.width * 0.025)/2; //set tick length relative to canvas width
    let Y_axis_length = (chart_canvas.height * 0.9); //set Y axis length relative to canvas height for future scaling
    let X_axis_length = (chart_canvas.width * 0.9); //let X axis length relative to canvas width for future scaling
-   let X_tick_offset = (X_axis_length / 14); //X ticks spacing divide by number of desired tics
-   let Y_tick_offset = (Y_axis_length / 10); //Y ticks spacing divide by number of desired tics
+   let X_tick_offset = (X_axis_length / (X_AXIS_NUMBEROFTICS-1)); //X ticks spacing divide by number of desired tics
+   let Y_tick_offset = (Y_axis_length / (Y_AXIS_NUMBEROFTICS-1)); //Y ticks spacing divide by number of desired tics
    let X_axis_margin = (chart_canvas.width-X_axis_length)/2; //margin between edge of canvas and start of chart relative for future scaling
    let Y_axis_margin = (chart_canvas.height - Y_axis_length)/2; //magin for Y axis chart for scaling
    let XY_axis_beginX = X_axis_margin; //set X axis zero X position relative to canvas for future scaling
@@ -92,40 +100,39 @@ function chart_it(TO_weight, TO_CofG, landing_weight, landing_CofG, WandB_warnin
 	  
 	  canvas_ctx.stroke();
 	  
-	  //fill in X axis ticks
-	  canvas_ctx.beginPath();
-	  canvas_ctx.lineWidth = 1;
-	  for(let i=1; i<15; i++ ){ //i<9 is the number of x tics.  9 for SBM but need 15 for EFH
-		 //draw the tick 
-		 canvas_ctx.beginPath();
-         canvas_ctx.moveTo(XY_axis_beginX + X_tick_offset*i, XY_axis_beginY + tick_length);
-	     canvas_ctx.lineTo(XY_axis_beginX + X_tick_offset*i, XY_axis_beginY - tick_length);	
-		 canvas_ctx.stroke();
-		 
-      }
-	  disp_count = 35;// starting number of x axis
-	  canvas_ctx.fillStyle = 'rgb(0,0,0)';	
-	  canvas_ctx.beginPath();
+
+	  
+	  //refine the X axis tic generation routine
+	  canvas_ctx.fillStyle = 'rgb(0,0,0)';
 	  canvas_ctx.textAlign = 'center';
 	  canvas_ctx.font = font_ticks;
-	  canvas_ctx.fillText('34', XY_axis_beginX, XY_axis_beginY + tick_length*2.1);
-	  for(let i=1; i<15; i++ ){//as above, for the number of tics on x axis
-         //draw the text number below the tick
-		 canvas_ctx.fillText(disp_count.toString(10), XY_axis_beginX + X_tick_offset*i, XY_axis_beginY + tick_length*2);
-		 disp_count = disp_count + 1;
-      }
+	  canvas_ctx.beginPath();
+	  canvas_ctx.lineWidth = 1;
 	  
+	  disp_count = X_AXIS_START_INCHES;// starting number of x axis
+	  for(let i=0; i<X_AXIS_NUMBEROFTICS; i++){
+        //draw the tic
+	    canvas_ctx.beginPath();	
+        canvas_ctx.moveTo(XY_axis_beginX + X_tick_offset*i, XY_axis_beginY + tick_length);
+	    canvas_ctx.lineTo(XY_axis_beginX + X_tick_offset*i, XY_axis_beginY - tick_length);	
+		canvas_ctx.stroke();
+        //draw the tic value number
+        canvas_ctx.fillText(disp_count.toString(10), XY_axis_beginX + X_tick_offset*i, XY_axis_beginY + tick_length*2);
+		disp_count = disp_count + X_AXIS_INCREMENT_VALUE;		
+	  }
+  
 	  //fill Y axis ticks
-	  disp_count = 1500;//y axis starting number
+	  disp_count = Y_AXIS_START_WEIGHT;//y axis starting number
 	  canvas_ctx.textAlign = 'end';
 	  canvas_ctx.fillText(disp_count.toString(10), XY_axis_beginX - tick_length, XY_axis_beginY); 
-	  for(let i=1; i<11; i++){
+	  for(let i=1; i<Y_AXIS_NUMBEROFTICS; i++){
 	     //draw the tick 
 		 canvas_ctx.beginPath();
          canvas_ctx.moveTo(XY_axis_beginX - tick_length, XY_axis_beginY - Y_tick_offset*i);
 	     canvas_ctx.lineTo(XY_axis_beginX + tick_length, XY_axis_beginY - Y_tick_offset*i);	
 		 canvas_ctx.stroke();
-		 disp_count = disp_count + 100;//this magic # is the lbs increase per tic on y axis
+		 //draw the tic value number	
+		 disp_count = disp_count + Y_AXIS_INCREMENT_VALUE;
 		 canvas_ctx.fillText(disp_count.toString(10), XY_axis_beginX - tick_length, XY_axis_beginY - Y_tick_offset*i);
 	  }
 
@@ -139,24 +146,24 @@ function chart_it(TO_weight, TO_CofG, landing_weight, landing_CofG, WandB_warnin
 	  canvas_ctx.beginPath();
 	  
 	  //bounding box of Normal catagory
-	  canvas_ctx.moveTo((35.0 *(X_tick_offset))-(34*(X_tick_offset)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((1500 * (Y_tick_offset/100)) - (1500*(Y_tick_offset/100))));
-	  canvas_ctx.lineTo((35.0 *(X_tick_offset))-(34*(X_tick_offset)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((1950 * (Y_tick_offset/100)) - (1500*(Y_tick_offset/100))));
-	  canvas_ctx.lineTo((38.5 *(X_tick_offset))-(34*(X_tick_offset)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((2300 * (Y_tick_offset/100)) - (1500*(Y_tick_offset/100))));
-	  canvas_ctx.lineTo((47.3 *(X_tick_offset))-(34*(X_tick_offset)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((2300 * (Y_tick_offset/100)) - (1500*(Y_tick_offset/100))));
-	  canvas_ctx.lineTo((47.3 *(X_tick_offset))-(34*(X_tick_offset)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((1500 * (Y_tick_offset/100)) - (1500*(Y_tick_offset/100))));
-	  canvas_ctx.lineTo((35.0 *(X_tick_offset))-(34*(X_tick_offset)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((1500 * (Y_tick_offset/100)) - (1500*(Y_tick_offset/100))));
+	  canvas_ctx.moveTo((35.0 *(X_tick_offset))-(X_AXIS_START_INCHES*(X_tick_offset)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((Y_AXIS_START_WEIGHT * (Y_tick_offset/Y_AXIS_INCREMENT_VALUE)) - (Y_AXIS_START_WEIGHT*(Y_tick_offset/Y_AXIS_INCREMENT_VALUE))));
+	  canvas_ctx.lineTo((35.0 *(X_tick_offset))-(X_AXIS_START_INCHES*(X_tick_offset)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((1950 * (Y_tick_offset/Y_AXIS_INCREMENT_VALUE)) - (Y_AXIS_START_WEIGHT*(Y_tick_offset/Y_AXIS_INCREMENT_VALUE))));
+	  canvas_ctx.lineTo((38.5 *(X_tick_offset))-(X_AXIS_START_INCHES*(X_tick_offset)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((2300 * (Y_tick_offset/Y_AXIS_INCREMENT_VALUE)) - (Y_AXIS_START_WEIGHT*(Y_tick_offset/Y_AXIS_INCREMENT_VALUE))));
+	  canvas_ctx.lineTo((47.3 *(X_tick_offset))-(X_AXIS_START_INCHES*(X_tick_offset)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((2300 * (Y_tick_offset/Y_AXIS_INCREMENT_VALUE)) - (Y_AXIS_START_WEIGHT*(Y_tick_offset/Y_AXIS_INCREMENT_VALUE))));
+	  canvas_ctx.lineTo((47.3 *(X_tick_offset))-(X_AXIS_START_INCHES*(X_tick_offset)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((Y_AXIS_START_WEIGHT * (Y_tick_offset/Y_AXIS_INCREMENT_VALUE)) - (Y_AXIS_START_WEIGHT*(Y_tick_offset/Y_AXIS_INCREMENT_VALUE))));
+	  canvas_ctx.lineTo((35.0 *(X_tick_offset))-(X_AXIS_START_INCHES*(X_tick_offset)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((Y_AXIS_START_WEIGHT * (Y_tick_offset/Y_AXIS_INCREMENT_VALUE)) - (Y_AXIS_START_WEIGHT*(Y_tick_offset/Y_AXIS_INCREMENT_VALUE))));
 	  canvas_ctx.stroke();
 	  canvas_ctx.fill();
 	  
 	  //bounding box of Utility category
 	  canvas_ctx.fillStyle = 'rgba(204, 230, 103, 0.2)';
 	  canvas_ctx.beginPath();
-	  canvas_ctx.moveTo((35.0 *(X_tick_offset))-(34*(X_tick_offset)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((1500 * (Y_tick_offset/100)) - (1500*(Y_tick_offset/100))));
-	  canvas_ctx.lineTo((35.0 *(X_tick_offset))-(34*(X_tick_offset)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((1950 * (Y_tick_offset/100)) - (1500*(Y_tick_offset/100))));
-	  canvas_ctx.lineTo((35.5 *(X_tick_offset))-(34*(X_tick_offset)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((2000 * (Y_tick_offset/100)) - (1500*(Y_tick_offset/100))));
-	  canvas_ctx.lineTo((40.5 *(X_tick_offset))-(34*(X_tick_offset)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((2000 * (Y_tick_offset/100)) - (1500*(Y_tick_offset/100))));
-	  canvas_ctx.lineTo((40.5 *(X_tick_offset))-(34*(X_tick_offset)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((1500 * (Y_tick_offset/100)) - (1500*(Y_tick_offset/100))));
-	  canvas_ctx.lineTo((35.0 *(X_tick_offset))-(34*(X_tick_offset)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((1500 * (Y_tick_offset/100)) - (1500*(Y_tick_offset/100))));
+	  canvas_ctx.moveTo((35.0 *(X_tick_offset))-(X_AXIS_START_INCHES*(X_tick_offset)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((Y_AXIS_START_WEIGHT * (Y_tick_offset/Y_AXIS_INCREMENT_VALUE)) - (Y_AXIS_START_WEIGHT*(Y_tick_offset/Y_AXIS_INCREMENT_VALUE))));
+	  canvas_ctx.lineTo((35.0 *(X_tick_offset))-(X_AXIS_START_INCHES*(X_tick_offset)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((1950 * (Y_tick_offset/Y_AXIS_INCREMENT_VALUE)) - (Y_AXIS_START_WEIGHT*(Y_tick_offset/Y_AXIS_INCREMENT_VALUE))));
+	  canvas_ctx.lineTo((35.5 *(X_tick_offset))-(X_AXIS_START_INCHES*(X_tick_offset)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((2000 * (Y_tick_offset/Y_AXIS_INCREMENT_VALUE)) - (Y_AXIS_START_WEIGHT*(Y_tick_offset/Y_AXIS_INCREMENT_VALUE))));
+	  canvas_ctx.lineTo((40.5 *(X_tick_offset))-(X_AXIS_START_INCHES*(X_tick_offset)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((2000 * (Y_tick_offset/Y_AXIS_INCREMENT_VALUE)) - (Y_AXIS_START_WEIGHT*(Y_tick_offset/Y_AXIS_INCREMENT_VALUE))));
+	  canvas_ctx.lineTo((40.5 *(X_tick_offset))-(X_AXIS_START_INCHES*(X_tick_offset)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((Y_AXIS_START_WEIGHT * (Y_tick_offset/Y_AXIS_INCREMENT_VALUE)) - (Y_AXIS_START_WEIGHT*(Y_tick_offset/Y_AXIS_INCREMENT_VALUE))));
+	  canvas_ctx.lineTo((35.0 *(X_tick_offset))-(X_AXIS_START_INCHES*(X_tick_offset)) + X_axis_margin, (Y_axis_margin+Y_axis_length) -((Y_AXIS_START_WEIGHT * (Y_tick_offset/Y_AXIS_INCREMENT_VALUE)) - (Y_AXIS_START_WEIGHT*(Y_tick_offset/Y_AXIS_INCREMENT_VALUE))));
 	  canvas_ctx.stroke();
 	  canvas_ctx.fill();
 	  
@@ -172,16 +179,15 @@ function chart_it(TO_weight, TO_CofG, landing_weight, landing_CofG, WandB_warnin
 	  
 	  //set display color to warning level
 	  canvas_ctx.fillStyle = display_colour;
-	  
-//xx working here	  
+	    
 
 	  //calculate Take Off point for plotting to the chart
 	  //X axis calculation X_tick_offset  = 90. 90/2 = 45 = 1 inch CG. so 10 in CG = (10 * (X_tick_offset/2)) - (8*(X_tick_offset/2)) + xmargin	  
 	  // from SBM => let TO_xvalue = (TO_CofG *(X_tick_offset/2))-(8*(X_tick_offset/2)) + X_axis_margin;
-	  let TO_xvalue = (TO_CofG *(X_tick_offset)) - (34*(X_tick_offset)) + X_axis_margin;
+	  let TO_xvalue = (TO_CofG *(X_tick_offset)) - (X_AXIS_START_INCHES*(X_tick_offset)) + X_axis_margin;
 	  //Y axis calculation (TO_weight * (Y_tick_offset/50)) - (750*(Y_tick_offset/50)) + Y_axis_margin *-1
 	  // from SMB => let TO_yvalue = (Y_axis_margin+Y_axis_length) -((TO_weight * (Y_tick_offset/50)) - (750*(Y_tick_offset/50)) );
-	  let TO_yvalue = (Y_axis_margin+Y_axis_length) -((TO_weight * (Y_tick_offset/100)) - (1500*(Y_tick_offset/100)) );
+	  let TO_yvalue = (Y_axis_margin+Y_axis_length) -((TO_weight * (Y_tick_offset/Y_AXIS_INCREMENT_VALUE)) - (Y_AXIS_START_WEIGHT*(Y_tick_offset/Y_AXIS_INCREMENT_VALUE)) );
 
 	  //Paint the take off Weight and Balance to the chart
 	  canvas_ctx.font = font_labels;
@@ -195,10 +201,10 @@ function chart_it(TO_weight, TO_CofG, landing_weight, landing_CofG, WandB_warnin
 	  //calculate Landing point for plotting to the chart
 	  //X axis calculation X_tick_offset  = 90. 90/2 = 45 = 1 inch CG. so 10 in CG = (10 * (X_tick_offset/2)) - (8*(X_tick_offset/2)) + xmargin	  
 	  // from SMB => let LDG_xvalue = (landing_CofG *(X_tick_offset/2))-(8*(X_tick_offset/2)) + X_axis_margin;
-	  let LDG_xvalue = (landing_CofG *(X_tick_offset))-(34*(X_tick_offset)) + X_axis_margin;
+	  let LDG_xvalue = (landing_CofG *(X_tick_offset))-(X_AXIS_START_INCHES*(X_tick_offset)) + X_axis_margin;
 	  //Y axis calculation (TO_weight * (Y_tick_offset/50)) - (750*(Y_tick_offset/50)) + Y_axis_margin *-1
 	  // from SBM => let LDG_yvalue = (Y_axis_margin+Y_axis_length) -((landing_weight * (Y_tick_offset/50)) - (750*(Y_tick_offset/50)) );
-	  let LDG_yvalue = (Y_axis_margin+Y_axis_length) -((landing_weight * (Y_tick_offset/100)) - (1500*(Y_tick_offset/100)) );
+	  let LDG_yvalue = (Y_axis_margin+Y_axis_length) -((landing_weight * (Y_tick_offset/Y_AXIS_INCREMENT_VALUE)) - (Y_AXIS_START_WEIGHT*(Y_tick_offset/Y_AXIS_INCREMENT_VALUE)) );
 	  
 	  //Paint the Landing Weight and Balance to the chart
 	  canvas_ctx.fillStyle = display_colour;
